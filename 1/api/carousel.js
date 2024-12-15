e8ea97db74ec12ce8718a02888ccf1e2153488fb
zhm1145 <1850724838@qq.com>
2024-12-15 19:15:53 +0800

    Delete 1 directory

--- 1/api/carousel.js
+++
@@ -1,6 +0,0 @@
-import request from '@/utils/request'
-
-//获取轮播图
-export const getCarousel = () => {
-    return request.get('v1/carousel');
-}
--- 1/api/search.js
+++
@@ -1,6 +0,0 @@
-import request from '@/utils/request'
-
-//搜索视频
-export const searchVideo = (keywords) => {
-    return request.get('v1/search?keywords=' + keywords);
-}
